febab795fcb4438740c59d3ac5a9f9667e25a0a9
shuzhouliu2008 <71595592+shuzhouliu2008@users.noreply.github.com>
2020-09-27 19:41:46 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,292 @@
-
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+function login(){
+  if (username == "20240201"){
+   if (password == "32751821"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240202"){
+   if (password == "87415256"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240203"){
+   if (password == "95154521"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240204"){
+   if (password == "28516629"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240205"){
+   if (password == "55279315"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240206"){
+   if (password == "66978461"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240207"){
+   if (password == "57393428"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240208"){
+   if (password == "42121103"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240209"){
+   if (password == "22784143"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240210"){
+   if (password == "42562621"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240211"){
+   if (password == "97105109"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240212"){
+   if (password == "31213228"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240213"){
+   if (password == "88869417"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240214"){
+   if (password == "75943096"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240215"){
+   if (password == "36113366"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240216"){
+   if (password == "71743141"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240217"){
+   if (password == "17696963"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240218"){
+   if (password == "71085417"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240219"){
+   if (password == "96062194"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240220"){
+   if (password == "99999993"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240221"){
+   if (password == "64681929"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240222"){
+   if (password == "69438338"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240223"){
+   if (password == "10000200"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240224"){
+   if (password == "17088443"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240225"){
+   if (password == "31755679"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240226"){
+   if (password == "88743260"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240227"){
+   if (password == "68666904"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240228"){
+   if (password == "72740098"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240229"){
+   if (password == "42728509"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240230"){
+   if (password == "56174553"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240231"){
+   if (password == "98163215"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240232"){
+   if (password == "21393511"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240233"){
+   if (password == "31846205"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240234"){
+   if (password == "25264432"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240235"){
+   if (password == "17941651"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240236"){
+   if (password == "94239646"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240237"){
+   if (password == "29230233"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240238"){
+   if (password == "57230562"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240239"){
+   if (password == "46148118"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240240"){
+   if (password == "77577246"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    if (username == "20240241"){
+   if (password == "60825209"){
+    window.location.href="https://eclassroom.github.io/cgi-bin/student/center/?loginKey=9230f2ihfq3ue8wv09woeqvh29vhqovewiha98aweivuowa8ego3hr983woaef892of3a83o9a82qfo8ewa"; 
+   }
+  }
+  else {
+    alert ("用户名或密码错误，请稍后重试或联系管理员！");
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+}
